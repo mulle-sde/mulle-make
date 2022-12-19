bc69eb2466a82f71a0a137d2003ebcebee2367ff
Nat! <nat@mulle-kybernetik.com>
2022-12-19 17:07:06 +0100

    fix package dependencies

--- .mulle/etc/project/formula-info.sh
+++ .mulle/etc/project/formula-info.sh
@@ -7,7 +7,7 @@ DESC="💄 Build projects with tools like cmake, meson, autoconf"
 LANGUAGE="bash"                # c,cpp, objc, bash ...
 # NAME="${PROJECT}"        # formula filename without .rb extension
 
-DEPENDENCIES='${MULLE_NAT_TAP}mulle-bashfunctions'
+DEPENDENCIES='${MULLE_NAT_TAP}mulle-env'
 
-DEBIAN_DEPENDENCIES="mulle-bashfunctions (>= 4.0.0)"
+DEBIAN_DEPENDENCIES="mulle-env"
 DEBIAN_RECOMMENDATIONS="build-essential"
